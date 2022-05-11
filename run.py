14d98d44417dad3d2ba3373255910f8ef5120c12
1047006050 <57098354+1047006050@users.noreply.github.com>
2022-05-12 01:11:42 +0800

    Update run.py

--- run.py
+++ run.py
@@ -57,7 +57,7 @@ if __name__ == "__main__":
         dk_info = json.loads(r.text)['data'][0]
         if dk_info['TBZT'] == "0":
             wid = dk_info['WID']
-            data = "?WID={}&IS_TWZC=1&CURR_LOCATION={}&ZJHSJCSJ={}&JRSKMYS=1&IS_HAS_JKQK=1&JZRJRSKMYS=1&SFZJLN=0".format(
+            data = "?WID={}&IS_TWZC=1&CURR_LOCATION={}&ZJHSJCSJ={}&JRSKMYS=1&IS_HAS_JKQK=1&JZRJRSKMYS=1&SFZJLN=1".format(
                 wid, curr_location, hs_date)
             url = URL_JKDK_APPLY + data
             log.info('正在打卡')
