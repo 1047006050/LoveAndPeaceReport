740d48d173a1427623d3a14a1a65c51e67dcf843
youyifei <1047006050@qq.com>
2022-05-21 09:56:07 +0800

    fix

--- run.py
+++ run.py
@@ -45,7 +45,7 @@ if __name__ == "__main__":
     log.info('登录成功！')
 
     r1 = auth.session.get(URL_UNREAD_LIST)
-    r1 = auth.session.get(URL_INDEX)
+
     for count in range(10):
         log.info('尝试获取打卡列表信息...')
         r = auth.session.get(URL_JKDK_LIST)
@@ -55,8 +55,7 @@ if __name__ == "__main__":
             continue
 
         dk_info = json.loads(r.text)['data'][0]
-        if 1:
-        #if dk_info['TBZT'] == "0":
+        if dk_info['TBZT'] == "0":
             wid = dk_info['WID']
             data = "?WID={}&IS_TWZC=1&CURR_LOCATION={}&ZJHSJCSJ={}&JRSKMYS=1&IS_HAS_JKQK=1&JZRJRSKMYS=1&SFZJLN=1".format(
                 wid, curr_location, "2022-05-20 16")
